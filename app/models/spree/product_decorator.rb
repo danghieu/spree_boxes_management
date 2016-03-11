c223a416878665722121ca14115b43fb4cf9aa5c
Dang Hieu <danghieu193@gmail.com>
2016-03-11 15:09:42 +0700

    fixing

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Product.class_eval do 
 	has_many :boxes , through: :box_products, :class_name => "Dish::Box"
-    has_many :box_products, dependent: :destroy, :class_name => "Dish::BoxProducts"
+    has_many :box_products, dependent: :destroy, :class_name => "Dish::BoxProduct"
     accepts_nested_attributes_for :box_products,
              :reject_if => :all_blank,
              :allow_destroy => true
